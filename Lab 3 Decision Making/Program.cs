f371d8d5ac411c0c9cd7c0c47b67d422725e3904
jamesjr19 <ruwartja@gmail.com>
2020-01-22 17:08:08 -0500

    Lab 3 Push 2

--- Lab 3 Decision Making/Program.cs
+++ Lab 3 Decision Making/Program.cs
@@ -5,8 +5,54 @@ namespace Lab_3_Decision_Making
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            string userContinue = "y";
+            while (userContinue != "n")
+            {
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Hi {name}, please enter a number between 1 and 100:");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput % 2 == 1 && userInput <= 100)
+                {
+                    Console.WriteLine($"Thanks {name}. {userInput} is odd");
+                }
+
+                else if (userInput % 2 == 0 && userInput <= 25)
+                {
+                    Console.WriteLine($"Thanks {name}. Even and less than 25.");
+                }
+
+                else if (userInput % 2 == 0 && userInput >= 26 && userInput <= 60)
+                {
+                    Console.WriteLine($"Thanks {name}. {userInput} is Even.");
+                }
+
+                else if (userInput % 2 == 0 && userInput > 60 && userInput <= 100)
+                {
+                    Console.WriteLine($"Thanks {name}. {userInput} is Even.");
+                }
+
+                else if (userInput > 100)
+                {
+                    Console.WriteLine("Invalid entry.");
+                }
+
+                userContinue = "";
+                while (userContinue != "y" && userContinue != "n")
+                {
+                    Console.WriteLine("Would you like to try again? y/n");
+                    userContinue = Console.ReadLine().ToLower();
+                }
+
+            }
+
+
+
+
+
         }
     }
 }
